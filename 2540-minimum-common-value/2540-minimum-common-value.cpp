590a9d6b44f15aab39e017db81745648b12e13ef
Yeswanth Reddy R <91588050+RachamallaYeswanthReddy@users.noreply.github.com>
2023-11-09 22:50:22 +0530

    Time: 157 ms (7.98%), Space: 81 MB (5.22%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,19 +1,25 @@
+
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()==0 || nums2.size()==0){
+        if (nums1.empty() || nums2.empty()) {
             return -1;
         }
+
         unordered_map<int, int> countMap;
-        for (int num : nums1) { countMap[num]++;}
-        vector<int> intersection;
+        int minCommon = INT_MAX; // Initialize with a large value
+
+        for (int num : nums1) {
+            countMap[num]++;
+        }
+
         for (int num : nums2) {
             if (countMap[num] > 0) {
-                intersection.push_back(num);
+                minCommon = min(minCommon, num);
                 countMap[num]--;
             }
         }
-        
-        return intersection.size() >= 1 ? intersection[0] : -1;
+
+        return (minCommon == INT_MAX) ? -1 : minCommon;
     }
 };
