b456a8c733f11dd0cf0121ac554f7ae768499dd6
Yeswanth Reddy R <91588050+RachamallaYeswanthReddy@users.noreply.github.com>
2023-11-12 22:25:15 +0530

    Time: 70 ms (73.73%), Space: 58.8 MB (58.30%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        std::unordered_map<int, int> freq, appendFreq;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (int num : nums) {
+            if (freq[num] == 0) {
+                continue;
+            } else if (appendFreq[num] > 0) {
+                appendFreq[num]--;
+                appendFreq[num + 1]++;
+            } else if (freq[num + 1] > 0 && freq[num + 2] > 0) {
+                freq[num + 1]--;
+                freq[num + 2]--;
+                appendFreq[num + 3]++;
+            } else {
+                return false;
+            }
+
+            freq[num]--;
+        }
+
+        return true;
+    }
+};
